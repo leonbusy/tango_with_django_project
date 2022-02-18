009f34be3c3c876ec056005b2e0e652f42649360
Hanliang Cai <2527345c@student.gla.ac.uk>
2022-02-18 17:59:41 +0000

    The ninth submmit: chapter10_finished

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -19,6 +19,8 @@ from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from datetime import datetime
+
 
 # def index(request):
 # return HttpResponse(r"<a href='/rango/about/'>About</a>"+'Rango says hey there partner!')
@@ -39,6 +41,7 @@ def index(request):
     # Place the list in our context_dict dictionary (with our boldmessage!)
     # that will be passed to the template engine.
     # try:
+    # request.session.set_test_cookie()
     category_list = Category.objects.order_by('-likes')[0:5]
     page_list = Page.objects.order_by('-views')[0:5]
 
@@ -46,17 +49,35 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
+
+    # visitor_cookie_handler(request)
+    # context_dict['visits'] = request.session['visits']
+
     # print(context_dict)
     # except:
     #     pass
     # Render the response and send it back!
-    return render(request, 'rango/index.html', context=context_dict)
+
+    # if request.session.test_cookie_worked():
+    #     print("TEST COOKIE WORKED!")
+        # request.session.delete_test_cookie()
+
+    response=render(request, 'rango/index.html', context=context_dict)
+    visitor_cookie_handler(request)
+    return response
 
 
 def about(request):
     context_dict = {
         'boldmessage': 'This tutorial has been put together by Hanliang.'}
-    return render(request, 'rango/about.html', context=context_dict)
+    
+    # if request.session.test_cookie_worked():
+    #     print("TEST COOKIE WORKED!")
+    #     request.session.delete_test_cookie()
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    response=render(request, 'rango/about.html', context=context_dict)
+    return response
 
 @login_required
 def show_category(request, category_name_slug):
@@ -255,4 +276,27 @@ def restricted(request):
     return render(request,'rango/restricted.html')
     # return HttpResponse("Since you're logged in, you can see this text!")
 
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
 
+# Updated the function definition
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request,
+    'last_visit',
+    str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+    '%Y-%m-%d %H:%M:%S')
+    # If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        # Update the last visit cookie now that we have updated the count
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        # Set the last visit cookie
+        request.session['last_visit'] = last_visit_cookie
+    # Update/set the visits cookie
+    request.session['visits'] = visits
--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -16,6 +16,7 @@ from re import M
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+SESSION_EXPIRE_AT_BROWSER_CLOSE=True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
